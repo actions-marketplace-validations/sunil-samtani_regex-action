e1717f6be3673909850546af283517f36f0d5981
sunil samtani <sunil.samtani@anquancapital.com>
2020-10-28 17:59:04 +0800

    [FIX] update  flags

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const core = require('@actions/core');
 
 try {
-  const regexPattern = core.getInput('regex_pattern');
-  const regexFlags = core.getInput('regex_flags');
-  const searchString = core.getInput('search_string');
+  let regexPattern = core.getInput('regex_pattern');
+  let regexFlags = core.getInput('regex_flags');
+  let searchString = core.getInput('search_string');
   if (!regexPattern) {
     core.setFailed('regex_pattern input is required');
     return;
   }
   if (!regexFlags) {
-    core.setFailed('regex_flags input is required');
+    regexFlags = '';
     return;
   }
   if (!searchString) {
     core.setFailed('search_string input is required');
     return;
   }
-  const regex = new RegExp(regexPattern, regexFlags);
-  const matches = searchString.match(regex);
+  let regex = new RegExp(regexPattern, regexFlags);
+  let matches = searchString.match(regex);
   if (!matches) {
     console.log('Could not find any matches');
     return;
